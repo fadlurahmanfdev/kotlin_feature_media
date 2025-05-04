f9457c7bcbd5fa3849b2f422621fefb01412c6a3
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2025-05-04 15:31:17 +0700

    feat(library): initialized ci-cd

--- pixmed/build.gradle.kts
+++ pixmed/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     id("maven-publish")
+
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 android {
@@ -34,25 +38,39 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release"){
-            groupId = "com.fadlurahmanfdev"
-            artifactId = "pixmed"
-            version = "0.0.2"
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 
-            afterEvaluate {
-                from(components["release"])
+    coordinates("com.fadlurahmanfdev", "pixmed", "0.0.1")
+
+    pom {
+        name.set("Pixmed")
+        description.set("Library to simplify media picker")
+        inceptionYear.set("2025")
+        url.set("https://github.com/fadlurahmanfdev/kotlin_feature_media/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
             }
+        }
+        developers {
+            developer {
+                id.set("fadlurahmanfdev")
+                name.set("Taufik Fadlurahman Fajari")
+                url.set("https://github.com/fadlurahmanfdev/")
+            }
+        }
+        scm {
+            url.set("https://github.com/fadlurahmanfdev/kotlin_feature_media/")
+            connection.set("scm:git:git://github.com/fadlurahmanfdev/kotlin_feature_media.git")
+            developerConnection.set("scm:git:ssh://git@github.com/fadlurahmanfdev/kotlin_feature_media.git")
         }
     }
 }
